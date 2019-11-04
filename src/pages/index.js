c2813c09a5b1b90cdd3bcf8d59ab616f3542810d
eclectic-coding <chuck@eclecticsaddlebag.com>
2019-11-03 21:39:37 -0500

    Add Layout component and refactor.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useSiteMetadata } from '../hooks/useSiteMetadata'
-
-const StyledH1 = styled.h1`
-color: rebeccapurple;
-`
+import React from 'react';
+import { Layout } from '../components/Layout';
 
 export default () => {
-  const { title, description } = useSiteMetadata()
   return (
     <>
-      <StyledH1>{title}</StyledH1>
-      <p>{description}</p>
+      <Layout />
     </>
-  )
+  );
 };
